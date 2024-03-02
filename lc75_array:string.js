d81241b7235e389065cac66b9b3e3d4f6469492b
e <ellen.jardon@gmail.com>
2024-03-02 13:01:25 -0600

    Greatest Common Divisor answer

--- lc75_array:string.js
+++ lc75_array:string.js
@@ -39,25 +39,47 @@
 //  * @param {string} word2
 //  * @return {string}
 //  */
-var mergeAlternately = function(word1, word2) {
-    let i = 0;
-    let mashup = "";
+// var mergeAlternately = function(word1, word2) {
+//     let i = 0;
+//     let mashup = "";
 
-    while ( i < word1.length || i < word2.length) {
-        if (word1.charAt(i) !==undefined) {
-        mashup += word1.charAt(i);
-        }
-        if (word1.charAt(i) !== undefined){
-        mashup += word2.charAt(i);
-        }
-        i++
-        console.log(mashup);
-        }
-    return console.log(mashup);
-};
+//     while ( i < word1.length || i < word2.length) {
+//         if (word1.charAt(i) !==undefined) {
+//         mashup += word1.charAt(i);
+//         }
+//         if (word1.charAt(i) !== undefined){
+//         mashup += word2.charAt(i);
+//         }
+//         i++
+//         console.log(mashup);
+//         }
+//     return console.log(mashup);
+// };
 
-mergeAlternately("abc", "xyz");
-mergeAlternately("abcd", "xyz");
-mergeAlternately("abc", "wxyz");
+// mergeAlternately("abc", "xyz");
+// mergeAlternately("abcd", "xyz");
+// mergeAlternately("abc", "wxyz");
 
 // ***************************************
+
+// Question 1071: Greatest Common Divisor 
+// review 
+
+var gcdOfStrings = function(str1, str2) {
+
+    if (str1 + str2 !== str2 + str1) {
+        return console.log("No pattern");
+    } else if (str1 == str2) {
+        return console.log(str1);
+    } else if (str1.length > str2.length) {
+        return console.log(str1.substring(str2.length));
+    }
+};
+
+
+gcdOfStrings("ABCABC", "ABC");
+gcdOfStrings("ABABAB", "ABAB");
+gcdOfStrings("LEET", "CODE");
+
+// ***************************************
+
