939288c167b34e0532e7d1f0c8957fd3ce232ba9
e <ellen.jardon@gmail.com>
2024-03-02 10:34:08 -0600

    merge strings alternately answer

--- lc75_array:string.js
+++ lc75_array:string.js
@@ -1,10 +1,10 @@
 // Question: 1768: Merge Strings Alternately
 
-/**
- * @param {string} word1
- * @param {string} word2
- * @return {string}
- */
+// /**
+//  * @param {string} word1
+//  * @param {string} word2
+//  * @return {string}
+//  */
 var mergeAlternately = function(word1, word2) {
     let i = 0;
     let word1Array = word1.split("");
@@ -12,9 +12,18 @@ var mergeAlternately = function(word1, word2) {
     let mashup = [];
 
     while ( i < word1.length || i < word2.length) {
+        if (word1Array[i] !==undefined) {
         mashup.push(word1Array[i]);
+        }
+        if (word2Array[i] !== undefined){
         mashup.push(word2Array[i]);
+        }
         i++
-    }
-    return console.log(mashup.join(""));
+        console.log(mashup);
+        }
+    return console.log((mashup.join("")));
 };
+
+mergeAlternately("abc", "xyz");
+
+// LC says out put is undefined? result shows it's a type of string...follow up with Sammie. Also ask about string vs string builder on run time
