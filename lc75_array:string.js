f907826012688addd138de9147803bbceb522e27
e <ellen.jardon@gmail.com>
2024-03-02 10:09:05 -0600

    new file with lc section name array:string added

--- lc75_array:string.js
+++ lc75_array:string.js
@@ -1,4 +1,20 @@
-const add = (a, b) => {
-    return a + b
-}
-console.log(add(4, 6))
+// Question: 1768: Merge Strings Alternately
+
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {string}
+ */
+var mergeAlternately = function(word1, word2) {
+    let i = 0;
+    let word1Array = word1.split("");
+    let word2Array = word2.split("");
+    let mashup = [];
+
+    while ( i < word1.length || i < word2.length) {
+        mashup.push(word1Array[i]);
+        mashup.push(word2Array[i]);
+        i++
+    }
+    return console.log(mashup.join(""));
+};
