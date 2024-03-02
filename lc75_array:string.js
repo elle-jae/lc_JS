6bc8b7060ecddb12a046d96fa41b159ee5b5b1a1
e <ellen.jardon@gmail.com>
2024-03-02 10:49:35 -0600

    alternate solution to Merge Strinng Alternately, used string methods

--- lc75_array:string.js
+++ lc75_array:string.js
@@ -5,25 +5,59 @@
 //  * @param {string} word2
 //  * @return {string}
 //  */
+// var mergeAlternately = function(word1, word2) {
+//     let i = 0;
+//     let word1Array = word1.split("");
+//     let word2Array = word2.split("");
+//     let mashup = [];
+
+//     while ( i < word1.length || i < word2.length) {
+//         if (word1Array[i] !==undefined) {
+//         mashup.push(word1Array[i]);
+//         }
+//         if (word2Array[i] !== undefined){
+//         mashup.push(word2Array[i]);
+//         }
+//         i++
+//         console.log(mashup);
+//         }
+//     return console.log((mashup.join("")));
+// };
+
+// mergeAlternately("abc", "xyz");
+// mergeAlternately("abcd", "xyz");
+// mergeAlternately("abc", "wxyz");
+
+// LC says out put is undefined? result shows it's a type of string...follow up with Sammie. Also ask about string vs string builder on run time
+
+// ***************************************
+
+// alternate solution to Merge String Alternately using strings
+
+// /**
+//  * @param {string} word1
+//  * @param {string} word2
+//  * @return {string}
+//  */
 var mergeAlternately = function(word1, word2) {
     let i = 0;
-    let word1Array = word1.split("");
-    let word2Array = word2.split("");
-    let mashup = [];
+    let mashup = "";
 
     while ( i < word1.length || i < word2.length) {
-        if (word1Array[i] !==undefined) {
-        mashup.push(word1Array[i]);
+        if (word1.charAt(i) !==undefined) {
+        mashup += word1.charAt(i);
         }
-        if (word2Array[i] !== undefined){
-        mashup.push(word2Array[i]);
+        if (word1.charAt(i) !== undefined){
+        mashup += word2.charAt(i);
         }
         i++
         console.log(mashup);
         }
-    return console.log((mashup.join("")));
+    return console.log(mashup);
 };
 
 mergeAlternately("abc", "xyz");
+mergeAlternately("abcd", "xyz");
+mergeAlternately("abc", "wxyz");
 
-// LC says out put is undefined? result shows it's a type of string...follow up with Sammie. Also ask about string vs string builder on run time
+// ***************************************
